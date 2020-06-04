71b253307b253d439afebb116e27869aeb78f8f4
JoseZ <josema.genomics@gmail.com>
2020-06-04 11:30:42 -0500

    Update: requirements errors

--- pkgs-requiremnts.R
+++ pkgs-requiremnts.R
@@ -1,8 +1,8 @@
 #! sudo apt install r-base-core
 
 #rutalib <- "/home/jzamora/storage/Rlibs"
-dir.create(rutalib, showWarnings = TRUE, recursive = TRUE)
-.libPaths(rutalib)
+#dir.create(rutalib, showWarnings = TRUE, recursive = TRUE)
+#.libPaths(rutalib)
 
 install.packages("openssl")
 if (!requireNamespace("devtools", quietly = TRUE))
