6937c02edef797a87fcef4e4afbe7dcee7c779b6
JoseZ <josema.genomics@gmail.com>
2020-06-10 20:14:59 -0500

    Update: intersections

--- install-miniconda.sh
+++ install-miniconda.sh
@@ -6,6 +6,6 @@ wget "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
 bash Miniconda3-latest-Linux-x86_64.sh -p $condadir -b -s
 rm -rf /tmp/Miniconda3-latest-Linux-x86_64.sh
 . $condadir/bin/activate
-conda install -y numpy pandas
+conda install -y numpy pandas seaborn
 easy_install trash-cli
 echo ". $condadir/bin/activate" >> ~/.bashrc
